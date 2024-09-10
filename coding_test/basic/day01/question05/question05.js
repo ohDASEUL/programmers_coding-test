4ea3453c499a4ebe30a15223568852e395b69ea1
ohDASEUL <comeseul312@gmail.com>
2024-09-10 16:52:05 +0900

    프로그래머스 코딩테스트 연습 - 코딩 기초 트레이닝 - 특수문자 출력하기

---
+++ coding_test/basic/day01/question05/question05.js
@@ -0,0 +1,10 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('close', function () {
+    console.log(`!@#$%^&*(\\'"<>?:;`)
+    // 역슬래시는 \\로 표기해야 함
+});
