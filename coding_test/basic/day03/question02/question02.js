499755962cad45c35104955c3ed70360d678dc55
ohDASEUL <comeseul312@gmail.com>
2024-09-17 18:13:22 +0900

    프로그래머스 코딩테스트 연습 - 코딩 기초 트레이닝 - 문자 리스트를 문자열로 반환하기

---
+++ coding_test/basic/day03/question02/question02.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    let answer = '';
+    for(let i = 0; i<arr.length; i++){
+        answer = arr.join('')
+    }
+    return answer;
+}
+
+// 간단하게 푼다면
+// function solution(arr) {
+//     return arr.join('');
+// }
