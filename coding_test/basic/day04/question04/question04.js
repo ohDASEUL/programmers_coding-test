26fedd3ca2ea1087c9e6b34406e05cae567ce1ab
ohDASEUL <comeseul312@gmail.com>
2024-09-18 20:37:48 +0900

    프로그래머스 코딩테스트 연습 - 코딩 기초 트레이닝 - 조건 문자열

---
+++ coding_test/basic/day04/question04/question04.js
@@ -0,0 +1,27 @@
+function solution(ineq, eq, n, m) {
+  if (ineq === ">" && eq === "=") {
+    if (n >= m) {
+      return 1;
+    } else {
+      return 0;
+    }
+  } else if (ineq === "<" && eq === "=") {
+    if (n <= m) {
+      return 1;
+    } else {
+      return 0;
+    }
+  } else if (ineq === ">" && eq === "!") {
+    if (n > m) {
+      return 1;
+    } else {
+      return 0;
+    }
+  } else if (ineq === "<" && eq === "!") {
+    if (n < m) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
