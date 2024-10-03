65111778227518a46b5d2ee07ff8c29b633bd0ba
ohDASEUL <comeseul312@gmail.com>
2024-10-03 11:12:58 +0900

    프로그래머스 코딩테스트 연습 - 코딩 기초 트레이닝 - 접미사인지 확인하기

---
+++ coding_test/basic/day09/question05/question.js
@@ -0,0 +1,9 @@
+function solution(my_string, is_suffix) {
+  let answer = [];
+  for (let i = 0; i < my_string.length; i++) {
+      answer.push(my_string.slice(i));
+  }
+  answer.sort();
+  
+  return Number(answer.includes(is_suffix));
+}
